75ea336025de825e2df6cf96c4b8abebcce2b3b5
koreaioi <koreaioi@gachon.ac.kr>
2026-05-19 17:20:21 +0900

    feat: 인가 담당 Interceptor 추가

--- src/main/java/roomescape/common/auth/config/AuthInterceptorConfig.java
+++ src/main/java/roomescape/common/auth/config/AuthInterceptorConfig.java
@@ -5,16 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import roomescape.common.auth.interceptor.AuthenticationInterceptor;
+import roomescape.common.auth.interceptor.AuthorizationInterceptor;
 
 @Configuration
 @RequiredArgsConstructor
 public class AuthInterceptorConfig implements WebMvcConfigurer {
 
     private final AuthenticationInterceptor authenticationInterceptor;
+    private final AuthorizationInterceptor authorizationInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authenticationInterceptor);
+        registry.addInterceptor(authenticationInterceptor).order(1);
+        registry.addInterceptor(authorizationInterceptor).order(2);
     }
 
 }
