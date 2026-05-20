b25f88ded0fde5745416bded8eaf2f2b4adea022
koreaioi <koreaioi@gachon.ac.kr>
2026-05-20 17:26:58 +0900

    feat: 웹, 모바일 앱 인증 구현 ReturnValueHandler로 분리

--- src/main/java/roomescape/member/controller/AuthController.java
+++ src/main/java/roomescape/member/controller/AuthController.java
@@ -1,13 +1,13 @@
 package roomescape.member.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.common.auth.annotation.SendToken;
 import roomescape.member.controller.dto.request.LoginDto;
+import roomescape.member.controller.dto.response.TokenDto;
 import roomescape.member.service.AuthService;
 
 @RestController
@@ -16,21 +16,12 @@ public class AuthController {
 
     private final AuthService authService;
 
-    // TODO 응답 고도화
+    @SendToken
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginDto dto) {
+    public ResponseEntity<TokenDto> login(@RequestBody LoginDto dto) {
         String token = authService.login(dto.toCommand());
-        ResponseCookie cookie = ResponseCookie.from("access_token", token)
-                .httpOnly(true)
-                .secure(true)
-                .path("/")
-                .build();
-
         return ResponseEntity.ok()
-                .header(HttpHeaders.AUTHORIZATION, token)
-                .header(HttpHeaders.SET_COOKIE, cookie.toString())
-                .header(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Authorization")
-                .body(token);
+                .body(TokenDto.from(token));
     }
 
 }
