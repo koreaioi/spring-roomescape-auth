897e66bfa196dd37ed61c44bbda04fc50f311e8f
koreaioi <koreaioi@gachon.ac.kr>
2026-05-20 11:09:53 +0900

    refactor: 회원가입시 validation 검증 추가

--- src/main/java/roomescape/member/controller/dto/request/MemberSaveDto.java
+++ src/main/java/roomescape/member/controller/dto/request/MemberSaveDto.java
@@ -1,9 +1,13 @@
 package roomescape.member.controller.dto.request;
 
+import roomescape.common.validation.annotation.NotBlank;
 import roomescape.member.service.dto.MemberSaveCommand;
 
 public record MemberSaveDto(
+        @NotBlank(message = "name을 입력해주세요.")
         String name,
+
+        @NotBlank(message = "비밀번호를 입력해주세요.")
         String password
 ) {
 
